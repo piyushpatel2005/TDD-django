50fd6fd557e96fc3c6a2b0d50eaea3f6b9137926
Piyush Patel <piyushpatel2005@gmail.com>
2018-01-13 14:41:37 +0530

    New form for list items

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
 class ItemFormTest(TestCase):
 
@@ -11,4 +11,11 @@ class ItemFormTest(TestCase):
         
     def test_form_renders_item_text_input(self):
         form = ItemForm()
-        self.fail(form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors['text'],
+            [EMPTY_ITEM_ERROR]
+        )
