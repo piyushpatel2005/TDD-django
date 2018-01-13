f2cdd95e00ede4e6c79c350e2bf98e6add85e8d1
Piyush Patel <piyushpatel2005@gmail.com>
2018-01-13 09:54:46 +0530

    Refactor hard-coded URLs from templates

--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,28 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 from lists.models import Item, List
-from django.core.exceptions import ValidationError
 
 def home_page(request):
     return render(request, 'home.html')
 
 
+def new_list(request):
+    list_ = List.objects.create()
+    item = Item(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
+    return redirect(f'/lists/{list_.id}/')
+
+
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     error = None
+
     if request.method == 'POST':
         try:
             item = Item(text=request.POST['item_text'], list=list_)
@@ -17,16 +31,5 @@ def view_list(request, list_id):
             return redirect(f'/lists/{list_.id}/')
         except ValidationError:
             error = "You can't have an empty list item"
+
     return render(request, 'list.html', {'list': list_, 'error': error})
-    
-def new_list(request):
-    list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = "You can't have an empty list item"
-        return render(request, 'home.html', {"error": error})   
-    return redirect(f'/lists/{list_.id}/')
