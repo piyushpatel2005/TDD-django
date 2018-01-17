2d6e782fec68a787263ad300f4b11346474c2a57
Piyush Patel <piyushpatel2005@gmail.com>
2018-01-17 17:18:43 +0530

    use forms advanced features

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -3,10 +3,12 @@ from django.test import TestCase
 from lists.models import Item, List
 
 
-class ItemModelsTest(TestCase):
-    def test_default_test(self):
+class ItemModelTest(TestCase):
+
+    def test_default_text(self):
         item = Item()
         self.assertEqual(item.text, '')
+
 
     def test_item_is_related_to_list(self):
         list_ = List.objects.create()
@@ -15,6 +17,7 @@ class ItemModelsTest(TestCase):
         item.save()
         self.assertIn(item, list_.item_set.all())
 
+
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
         item = Item(list=list_, text='')
@@ -22,20 +25,22 @@ class ItemModelsTest(TestCase):
             item.save()
             item.full_clean()
 
+
     def test_duplicate_items_are_invalid(self):
         list_ = List.objects.create()
         Item.objects.create(list=list_, text='bla')
         with self.assertRaises(ValidationError):
             item = Item(list=list_, text='bla')
             item.full_clean()
-            # item.save()
-    
+
+
     def test_CAN_save_same_item_to_different_lists(self):
         list1 = List.objects.create()
         list2 = List.objects.create()
         Item.objects.create(list=list1, text='bla')
         item = Item(list=list2, text='bla')
-        item.full_clean()       # should not raise error
+        item.full_clean()  # should not raise
+
 
     def test_list_ordering(self):
         list1 = List.objects.create()
@@ -47,11 +52,15 @@ class ItemModelsTest(TestCase):
             [item1, item2, item3]
         )
 
+
     def test_string_representation(self):
         item = Item(text='some text')
         self.assertEqual(str(item), 'some text')
 
+
+
 class ListModelTest(TestCase):
+
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
