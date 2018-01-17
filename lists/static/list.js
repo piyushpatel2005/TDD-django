448f400ab3c83520790a7efa1fa7e76d36566cd5
Piyush Patel <piyushpatel2005@gmail.com>
2018-01-17 19:20:43 +0530

    update typo

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,6 +1,6 @@
 window.Superlists = {};
 window.Superlists.initialize = function () {
-    $('input[name="text"]').on('keypress', function () {
-        $('.has-error').hide();
-    });
+  $('input[name="text"]').on('keypress', function () {
+    $('.has-error').hide();
+  });
 };
